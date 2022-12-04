d9bee599627c4f186b750635b10bef55476de856
AndriiDenysenko <st4r1r@gmail.com>
2022-12-04 21:56:48 +0200

    Initial django setup

---
+++ settings/docker.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nov-11',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
