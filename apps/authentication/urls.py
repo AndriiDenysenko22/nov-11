0d2566459d7fbbd39ef806625c0736fc28922275
AndriiDenysenko <st4r1r@gmail.com>
2023-01-06 16:59:34 +0200

    fixed prospector

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -5,10 +5,9 @@ from apps.authentication.views import LoginView, AccountView, LogoutView, Regist
 
 app_name = 'authentication'
 
-urlpatterns=[
+urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('account/', AccountView.as_view(), name='account'),
     path('register/', RegisterView.as_view(), name='register')
 ]
-
